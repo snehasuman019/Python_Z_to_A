18c02b1e2c760effbd5995e7f2d7a7510be86122
[sneha suman] <snehasuman1119@gmail.com>
2025-12-14 13:02:09 +0530

    variables

---
+++ 01.basics/variables.py
@@ -0,0 +1,18 @@
+# Topic: Variables and Data Types
+# Author: Sneha Suman
+
+# Integer
+age = 20
+print(age)
+
+# Float
+height = 5.4
+print(height)
+
+# String
+name = "Sneha"
+print(name)
+
+# Boolean
+is_student = True
+print(is_student)
