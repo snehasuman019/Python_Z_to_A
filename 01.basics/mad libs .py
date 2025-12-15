69428f9120ccc11f2348e94e37b151888a0444b1
[sneha suman] <snehasuman1119@gmail.com>
2025-12-15 19:11:14 +0530

    Mad libs games

---
+++ 01.basics/mad libs .py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+person = input("Enter a person's name: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + person)
