4a900f0feb950fa5a7f00d1caee3001d9c92b5be
[sneha suman] <snehasuman1119@gmail.com>
2025-12-15 23:32:28 +0530

    2D and nested loops

---
+++ 04.Control Flow/nested.py
@@ -0,0 +1,14 @@
+#2D and nested loops
+
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+print(number_grid[0][0])  #accessing element at row 0, column 0
+
+for row in number_grid:
+    print(row)
+    for col in row:
+        print(col)
