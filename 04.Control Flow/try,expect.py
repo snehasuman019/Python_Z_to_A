e2b10b53c8de633c8422f6d719cd7bf1d899aafd
[sneha suman] <snehasuman1119@gmail.com>
2025-12-16 23:18:50 +0530

    Try / Except

--- 04.Control Flow/try,expect.py
+++ 04.Control Flow/try,expect.py
@@ -5,8 +5,19 @@ print(number)
 #try except block to handle errors
 #it blocks the code from crashing
 
+
+# try:
+#     number = int(input("Enter a number: "))
+#     print(number)
+# except:
+#     print("Invalid Input")
+
+
 try:
+    value = 10 / 0
     number = int(input("Enter a number: "))
     print(number)
-except:
-    print("Invalid Input")
+except ZeroDivisionError as err:
+    print("err")
+except ValueError:
+    print("Invalid input")
