cbd1f14ef2b4a68d8e2f6c35c695a0bbf166ad9b
[sneha suman] <snehasuman1119@gmail.com>
2025-12-17 23:17:06 +0530

    Classes

--- Classes and Objects/main.py
+++ Classes and Objects/main.py
@@ -9,4 +9,5 @@ from student import Student
 student1 = Student("Jim", "Business", 3.1, False)
 student2 = Student("Pam", "Art", 2.5, True)
 print(student1.name)
-print(student2.gpa)
+print(student2.gpa) 
+print(student1.gpa)
