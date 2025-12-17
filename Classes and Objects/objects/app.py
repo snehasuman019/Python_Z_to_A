a5fda65ea4b426d36a29a0fe3114ba08d1dbae48
[sneha suman] <snehasuman1119@gmail.com>
2025-12-17 23:57:30 +0530

    Classes and Objects

---
+++ Classes and Objects/objects/app.py
@@ -0,0 +1,7 @@
+from student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 3.8, False)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
