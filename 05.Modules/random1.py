01dae05a9d9145c0f1c2975eeab06ca1cc6b48a2
[sneha suman] <snehasuman1119@gmail.com>
2025-12-21 18:29:31 +0530

    Modules

--- 05.Modules/random1.py
+++ 05.Modules/random1.py
@@ -34,3 +34,27 @@ print(z)
 
 y=secrets.randbits(4)
 print(y)
+
+mylist1 = list("ABCDEFGH")
+s=secrets.choice(mylist1)
+print(s)
+
+
+
+import numpy as np
+f=np.random.randint(0,10, (3,4))
+print(f)
+
+print("*********")
+
+arr = np.array([[1,2,3], [4,5,6], [7,8,9]])
+print(arr)
+np.random.shuffle(arr)
+print(arr)
+
+
+
+np.random.seed(1)
+print(np.random.rand(3,3))
+# np.random.seed(1)
+# print(np.random.rand(3,3))
