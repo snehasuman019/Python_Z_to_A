e5af184d83fe71d2df1cdec6451b7973323f278b
[sneha suman] <snehasuman1119@gmail.com>
2025-12-31 00:22:05 +0530

    Generators

--- 08.Advanced Python/Generators.py
+++ 08.Advanced Python/Generators.py
@@ -19,3 +19,19 @@ print(sum(g))
 
 # for i in g:
 #     print(i)
+
+def countdown(num):
+    print("Starting")
+    while num > 0:
+        yield num 
+        num -= 1
+cd = countdown(4)
+value = next(cd)
+print(value)
+print(next(cd))
+print(next(cd))
+print(next(cd))
+
+
+# value = next(g)
+# print(value)
