90fdaa76d981f7399901fa67c9552eb3eaafa7cf
[sneha suman] <snehasuman1119@gmail.com>
2025-12-31 00:28:55 +0530

    Generators

--- 08.Advanced Python/Generators.py
+++ 08.Advanced Python/Generators.py
@@ -35,3 +35,30 @@ print(next(cd))
 
 # value = next(g)
 # print(value)
+
+def first(n):
+    nums = []
+    num = 0
+    while num < n:
+        nums.append(num)
+        num += 1
+    return nums
+def first_generator(n):
+    num = 0
+    while num < n:
+        yield num
+        num += 1
+# print(first(10))
+# print(sys.getsizeof(first(1000000))) # size in bytes
+
+
+#Fibonacci
+def fibonacci(limit):
+    a, b = 0, 1
+    # for _ in range(n):
+    while a < limit:
+        yield a
+        a, b = b, a + b
+fib = fibonacci(10)
+for i in fib:
+    print(i)
