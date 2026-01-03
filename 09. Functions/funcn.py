71c2a85d4301e9d20a97f822ec531896f3e20c1f
[sneha suman] <snehasuman1119@gmail.com>
2026-01-04 00:02:05 +0530

    Document strings in functions

--- 09. Functions/funcn.py
+++ 09. Functions/funcn.py
@@ -8,3 +8,11 @@ def addNumbers(num1, num2):
     return num1 + num2  
 result = addNumbers(5, 10)
 print(result)
+
+# Doc Strings
+def printSuccess2():
+    '''This func is doing nothing except printing a message
+    that message is hellow'''
+    print("Hellow")
+printSuccess2()
+print(printSuccess2.__doc__)
