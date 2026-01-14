f8e7f72636cd3bfa1a48e19dc358aa8c4c0c3e17
[sneha suman] <snehasuman1119@gmail.com>
2026-01-14 23:09:28 +0530

    encapsulation

---
+++ 06.Classes and Objects/encapsulation.py
@@ -0,0 +1,20 @@
+# Data hiding
+# Access control
+# Secure classes
+# Encapsulation = Protect data
+
+# encapsulation.py
+
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance   # private variable
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = Account(1000)
+acc.deposit(500)
+print(acc.get_balance())
