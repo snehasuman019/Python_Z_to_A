f84940efeaa8772efa0183605808b9f40b017044
[sneha suman] <snehasuman1119@gmail.com>
2026-01-14 23:06:22 +0530

    Inheritance

--- 07.Inheritance/main.py
+++ 07.Inheritance/main.py
@@ -6,3 +6,17 @@ my_chef.make_special_dish()
 
 my_chinese_chef = ChineseChef()
 my_chinese_chef.make_special_dish()
+
+# inheritance.py
+
+class Person:
+    def speak(self):
+        print("Person can speak")
+
+class Student(Person):
+    def study(self):
+        print("Student can study")
+
+s = Student()
+s.speak()
+s.study()
