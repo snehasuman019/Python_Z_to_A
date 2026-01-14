fd158097ae43362d2c5a26883b7fa9a282a045c6
[sneha suman] <snehasuman1119@gmail.com>
2026-01-14 23:10:09 +0530

    encap

--- 06.Classes and Objects/encapsulation.py
+++ 06.Classes and Objects/encapsulation.py
@@ -18,3 +18,21 @@ class Account:
 acc = Account(1000)
 acc.deposit(500)
 print(acc.get_balance())
+
+
+
+# practice.py
+
+class Vehicle:
+    def __init__(self, brand):
+        self.brand = brand
+
+    def move(self):
+        print("Vehicle moves")
+
+class Car(Vehicle):
+    def move(self):
+        print(self.brand, "car drives")
+
+c = Car("Honda")
+c.move()
