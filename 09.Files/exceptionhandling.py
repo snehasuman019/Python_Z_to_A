529e86a50c04b5eab86a410101b333053b4e96b6
[sneha suman] <snehasuman1119@gmail.com>
2026-01-15 23:13:04 +0530

    exception handling

---
+++ 09.Files/exceptionhandling.py
@@ -0,0 +1,16 @@
+# try:
+#     x = int(input())
+#     print(10 / x)
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
+# except ValueError:
+#     print("Invalid input")
+
+try:
+    print("Try block")
+except:
+    print("Error")
+else:
+    print("No error")
+finally:
+    print("Always runs")
